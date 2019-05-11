421170ef9c463a0e360bf81641cfc9f852fb51e2
biancafaial <biancafaial@gmail.com>
2019-05-10 22:18:50 -0300

    Site com API filmes

--- semana04/exe-movies/script.js
+++ semana04/exe-movies/script.js
@@ -1,1 +1,51 @@
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=e2681bcc')
+let button = document.querySelector('.btn-search-movie');
+let movies = document.querySelector('.movies');
+ 
+button.addEventListener('click', (busca)=>{
+    busca.preventDefault();
+    //limpar a pagina para outra busca sem precisar recarregar
+    movies.inneHTML = ' '
+
+    fetch('http://www.omdbapi.com/?s='+movie.value+'&apikey=e2681bcc')
+
+    .then((response) => {
+        return response.json();
+    })
+    .then((infos) => {
+        console.log(movie.value)
+        infos.Search.forEach(filme => {
+            let card = document.createElement('div');
+            card.setAttribute('class', 'box');
+            card.setAttribute('data-id', filme.imdbID);
+            movies.appendChild(card);
+
+            let Poster = document.createElement('img');
+            Poster.src = filme.Poster;
+            card.appendChild(Poster);
+
+            let cardInfor = document.createElement('div');
+            cardInfor.setAttribute('class', 'box-divider')
+            card.appendChild(cardInfor);
+
+            let paragrafo = document.createElement('p');
+            cardInfor.appendChild(paragrafo);
+
+            let titulo = document.createElement('span')
+            titulo.textContent = filme.Title +" ";
+            paragrafo.appendChild(titulo);
+
+            let ano = document.createElement('span');
+            ano.textContent ='('+ filme.Year+')';
+            paragrafo.appendChild(ano);
+
+            let tipo = document.createElement('p')
+            tipo.textContent = filme.Type;
+            cardInfor.appendChild(tipo);
+
+        })
+    })
+    .catch((erro) => {
+        console.log(erro)
+    
+    })
+})
